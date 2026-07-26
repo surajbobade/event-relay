170a2973fae91d3b2e8d6384333b7821eea3c2ba
Suraj Bobade <suraj@192.168.1.4>
2026-07-26 21:21:05 +0530

    fix: add vercel domain in cor

--- server/src/main.ts
+++ server/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
     app.enableCors({
-        origin: 'http://localhost:5173',
+        origin: ['http://localhost:5173', 'https://request-studio.vercel.app'],
         credentials: true,
     });
 
@@ -20,7 +20,7 @@ async function bootstrap() {
             transform: true,
         }),
     );
-  
+
     await app.listen(process.env.PORT ?? 3000);
 }
 
