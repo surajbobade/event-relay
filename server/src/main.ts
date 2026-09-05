061d76b7dd393664d9d865ff7838c0113d0a259d
Suraj Bobade <suraj.b@indecab.com>
2026-09-05 23:23:52 +0530

    fix: update cors origin

--- server/src/main.ts
+++ server/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
     app.enableCors({
-        origin: ['http://localhost:5173', 'https://eventrelay.in'],
+        origin: ['http://localhost:5173', 'https://app.eventrelay.in'],
         credentials: true,
     });
 
